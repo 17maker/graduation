5048fbb5c5f30cf1e6c5de56b49bc8eea95c6b0c
agoomon9126 <agoomon9126@naver.com>
2022-03-16 20:43:25 +0900

    first commit

---
+++ app/src/main/java/com/example/lasbetalk/LoginActivity.kt
@@ -0,0 +1,34 @@
+package com.example.lasbetalk
+import android.content.Intent
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.*
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+
+private lateinit var auth: FirebaseAuth
+
+class LoginActivity: AppCompatActivity() {
+
+    private fun updateUI(user: FirebaseUser?) {
+
+    }
+
+    public override fun onStart() {
+        super.onStart()
+        // Check if user is signed in (non-null) and update UI accordingly.
+        val currentUser = auth.currentUser
+        if(currentUser != null){
+            reload();
+        }
+    }
+
+    private fun reload() {
+
+    }
+}
+
